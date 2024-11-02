e066a06bd6b7810682edf704d558a64b087f1eea
Julio Gema <julicious@Julios-MacBook-Air.local>
2024-11-02 22:41:04 +0700

    feat: enable and disable raw mode

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module text-editor-go
 
 go 1.22.3
+
+require (
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/term v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"os/exec"
+)
 
-func main(){
-	fmt.Println("hello world")
+func main() {
+	enableRawMode()
+	defer disableRawMode()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		read, err := reader.ReadByte()
+		if err != nil || read == byte('q') {
+			return
+		}
+	}
+}
+
+func enableRawMode() {
+	command := exec.Command("stty", "raw")
+	command.Stdin = os.Stdin
+	err := command.Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func disableRawMode() {
+	command := exec.Command("stty", "-raw")
+	command.Stdin = os.Stdin
+	err := command.Run()
+	if err != nil {
+		panic(err)
+	}
 }
